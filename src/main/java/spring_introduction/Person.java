7805a31ce43dc8aa771dad4938049c24577b3fe4
Denis Larikov <serzhantov-02@mail.ru>
2023-09-25 17:30:23 +0500

    annotation

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -1,13 +1,25 @@
 package spring_introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
 public class Person {
+    @Autowired
+    @Qualifier("dog")
     private  Pet pet;
+    // @Value("Larikov")
+    @Value("${person.surname}")
     private String surname;
 
 
-    public Person() {
-
-    }
+//    @Autowired
+//    public Person(Pet pet) {
+//        System.out.println("Person");
+//        this.pet = pet;
+//    }
 
     public String getSurname() {
         return surname;
@@ -17,6 +29,7 @@ public class Person {
         this.surname = surname;
     }
 
+    // @Autowired
     public void setPet(Pet pet) {
         this.pet = pet;
     }
--- src/main/java/spring_introduction/Test1.java
+++ src/main/java/spring_introduction/Test1.java
@@ -3,10 +3,13 @@ package spring_introduction;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test1 {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.context.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.context2.xml");
 
-        Person person = context.getBean("myPerson", Person.class);
+        Person person = context.getBean("personBean", Person.class);
         person.callYourPet();
+//        Cat cat = context.getBean("catBean", Cat.class);
+//
+//        cat.say();
 
         context.close();
     }
