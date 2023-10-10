a2ab151808324ef0142d2615922b450deda49b6e
Denis Larikov <serzhantov-02@mail.ru>
2023-10-10 14:55:26 +0500

    lol

--- src/main/java/spring_introduction/Test1.java
+++ src/main/java/spring_introduction/Test1.java
@@ -1,10 +1,15 @@
 package spring_introduction;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test1 {
     public static void main(String[] args) {
-        MathHomework mathHomework = new MathHomework();
-        System.out.println(mathHomework.findMinInArray());
+        Player bot = new Player();
+
+        Player alex = new Player(VARIANTS.SCISSORS, "Alex");
+
+// Получаем результат
+
+        System.out.println(bot.whoWins(bot, alex));
+
+
     }
 }
